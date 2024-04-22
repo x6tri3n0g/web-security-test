6b8e2eb1ef6d27643886368f5f5bc7591a20516c
xtring <hh940630@gmail.com>
2024-04-22 23:31:12 +0900

    feat: request header에 X-Lang 요소 추가

--- routes/api.js
+++ routes/api.js
@@ -2,10 +2,18 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
+  res.setHeader("X-Timestamp", Date.now());
   let message = req.query.message;
+  const lang = req.headers["x-lang"];
 
   if (message === "") {
     res.status(400);
+    message = "message 값이 비었음";
+  }
+
+  if (lang === "en") {
+    message = "message is empty.";
+  } else {
     message = "message 값이 비었음";
   }
 
